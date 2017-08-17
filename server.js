0f6d6b3e30fad9a919880055189a4e8dd9923f5c
Kostya <mr.ratusha@gmail.com>
2017-08-17 10:47:04 +0300

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var serverFactory = require('./node_modules/spa-server');
 var server = serverFactory.create({
   path: './build',
   port: process.env.PORT || 3000,
-  fallback: './build/index.html'
+  fallback: 'index.html'
 });
 
 server.start();
